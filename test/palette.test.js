e71b60ae966c864e476f1608ba2ae0bd214648f9
JamesToohey <james.toohey@gmail.com>
2019-07-06 06:51:51 +1200

    fix palette test

--- test/palette.test.js
+++ test/palette.test.js
@@ -4,7 +4,7 @@ import Pigment, { Palette } from '../src/index';
 describe('Palette instantiation', () => {
     test('it returns source colour and complementary colour', () => {
         const colour = Pigment('#47FF66');
-        const palette = Palette();
+        const palette = Palette(colour);
         expect(palette.primary).toEqual(colour);
         expect(palette.complementary).toEqual(colour.complementary());
     });
